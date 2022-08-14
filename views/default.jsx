92e767d38fec6b32926c30bb5da9d54e14c3c31d
Ian <naberbach@gmail.com>
2022-08-14 12:37:14 -0400

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -1,18 +1,36 @@
-const React = require('react')
+const React = require("react");
 
-function Def (html) {
-    return (
-        <html>
-            <head>
-                <title>Title</title>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"></link>
-                <link rel="stylesheet" href="/css/style.css"></link>
-            </head>
-            <body>
-                {html.children}
-            </body>
-        </html>
-    )
-  }
+function Def(html) {
+  return (
+    <html>
+      <head>
+        <title>Title</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+          integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+          crossorigin="anonymous"
+        ></link>
+        <link rel="stylesheet" href="/css/style.css"></link>
+      </head>
+      <body>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/places">Places</a>
+            </li>
+            <li>
+              <a href="/places/new">Add Place</a>
+            </li>
+          </ul>
+        </nav>
+        {html.children}
+      </body>
+    </html>
+  );
+}
 
-module.exports = Def
+module.exports = Def;
