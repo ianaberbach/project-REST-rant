157f8b0ec428343277905ea2199160910bd2325c
Ian <naberbach@gmail.com>
2022-08-14 12:21:55 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,24 +1,30 @@
 const router = require('express').Router()
+const places = require('../models/places')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/rest1.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/rest2.jpg'
-      }]      
+        
     res.render('places/index', {places})
 })
 
 router.get('/new', (req,res) => {
   res.render('places/new')
 })
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
 
 module.exports = router
---
+++ models/places.js
@@ -0,0 +1,15 @@
+let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/rest1.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/rest2.jpg'
+  }] 
+
+  module.exports = places
